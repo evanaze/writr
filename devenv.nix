e161f84bb1dbeaf825a611df5d9c44543a00d0c7
Evan Azevedo <me@evanazevedo.com>
2026-08-01 11:01:17 -0600

    adding node to devenv

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,10 @@
 {...}: {
   # https://devenv.sh/languages/
   languages.go.enable = true;
-  languages.javascript.npm.enable = true;
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
 
   # https://devenv.sh/scripts/
   scripts.build.exec = "go build -o writr .";
